6aba5739de3724cd6eeb938897961c87bf4539d9
Zach Hoeken <hoeken@gmail.com>
2020-06-15 19:45:32 -0500

    forgot some config.

--- bin/user/hoekwindledmatrix.py
+++ bin/user/hoekwindledmatrix.py
@@ -70,12 +70,21 @@ def _mps_to_knot(v):
 	from_t = (v, 'meter_per_second', 'group_speed')
 	return weewx.units.convert(from_t, 'knot')[0]
 
+LED_ROWS = 11
+LED_COLS = 44
+LED_COUNT = LED_ROWS * LED_COLS		# Number of LED pixels.
+LED_PIN = 18		  # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10		  # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False	# True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0	   # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 class HoekWindLEDMatrix(weewx.engine.StdPrint):
 
 	def __init__(self, engine, config_dict):
 		loginf("hoekwind init")
-
+		
 		# Create NeoPixel object with appropriate configuration.
 		self.strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 		# Intialize the library (must be called once before other functions).
